c1cf2453238b3de9e48341ba0be2fb3dc30c1e01
Lewis T <lewisfalconer@outlook.com>
2020-11-19 14:47:47 +0000

    Started adding RDS terraform modules

---
+++ Terraform/Modules/RDS/Testing/main.tf
@@ -0,0 +1,11 @@
+resource "aws_db_instance" "TestingDB" {
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "TestingDB"
+  username             = "FalconTest"
+  password             = "MWAMDATABASE"
+  parameter_group_name = "default.mysql5.7"
+}
